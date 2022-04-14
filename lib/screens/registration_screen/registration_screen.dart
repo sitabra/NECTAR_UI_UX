78ab3d474f263114a66c339530833ee44a0ad6f4
sitabra <sitabra.das@cbnits.com>
2022-04-14 17:52:53 +0530

    header image of registration page

--- lib/screens/registration_screen/registration_screen.dart
+++ lib/screens/registration_screen/registration_screen.dart
@@ -10,6 +10,7 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: Column(
         children: [
           Container(
@@ -18,7 +19,7 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
             width: double.infinity,
             decoration: BoxDecoration(
               image: DecorationImage(
-                image: NetworkImage("https://media.istockphoto.com/photos/fresh-raw-vegetable-variation-in-a-paper-shopping-bag-on-a-white-top-picture-id1226363446?k=20&m=1226363446&s=170667a&w=0&h=JhB9PfRjrcmnRGDcayoc2ZaSGOX-091ACcnIl3COtCE="),
+                image: NetworkImage("https://t3.ftcdn.net/jpg/02/46/02/78/360_F_246027822_NwEoRdOEJZ9K62jkO0Stk39yDAt919hE.jpg"),
                 fit: BoxFit.cover,
               )
             ),
