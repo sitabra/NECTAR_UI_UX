5b62dbe0d8355c1cbba0b3f988b31490ade66571
sitabra <sitabra.das@cbnits.com>
2022-04-14 17:46:18 +0530

    registration page image

--- lib/screens/registration_screen/registration_screen.dart
+++ lib/screens/registration_screen/registration_screen.dart
@@ -13,9 +13,15 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
       body: Column(
         children: [
           Container(
-            color: Colors.green,
-            height: 200,
+            // color: Colors.green,
+            height: 250,
             width: double.infinity,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: NetworkImage("https://media.istockphoto.com/photos/fresh-raw-vegetable-variation-in-a-paper-shopping-bag-on-a-white-top-picture-id1226363446?k=20&m=1226363446&s=170667a&w=0&h=JhB9PfRjrcmnRGDcayoc2ZaSGOX-091ACcnIl3COtCE="),
+                fit: BoxFit.cover,
+              )
+            ),
           )
         ],
       ),
