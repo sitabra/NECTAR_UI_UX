e6d036124c381a57ba7f20ce6b73efa269359ff4
sitabra <sitabra.das@cbnits.com>
2022-04-14 18:27:02 +0530

    image and text in registration page

--- lib/screens/registration_screen/registration_screen.dart
+++ lib/screens/registration_screen/registration_screen.dart
@@ -11,20 +11,30 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Column(
-        children: [
-          Container(
-            // color: Colors.green,
-            height: 250,
-            width: double.infinity,
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: NetworkImage("https://t3.ftcdn.net/jpg/02/46/02/78/360_F_246027822_NwEoRdOEJZ9K62jkO0Stk39yDAt919hE.jpg"),
-                fit: BoxFit.cover,
-              )
+      body: Container(
+        child: Column(
+          children: [
+            Container(
+              height: 250,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: NetworkImage("https://t3.ftcdn.net/jpg/02/46/02/78/360_F_246027822_NwEoRdOEJZ9K62jkO0Stk39yDAt919hE.jpg"),
+                  fit: BoxFit.cover,
+                )
+              ),
             ),
-          )
-        ],
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Row(
+                children: [
+                  Text("Get your groceries\nwith Nectar",
+                  style: TextStyle(fontSize: 20),),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
